788d50d1cca9823834a462c228bc1345fecdf881
kj-agg <kieran.j.agg@gmail.com>
2025-12-17 20:16:35 +0000

    Deploying to gh-pages from @ kj-agg/kj-agg.github.io@a96bcda714b2833725713238cbb57bf60eda10e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "A recent version of my CV can be downloaded from this page.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
